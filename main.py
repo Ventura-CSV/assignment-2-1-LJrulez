247eff5e4d3b253fda2f0405895d83e2b7c612e2
your <your>
2023-08-26 15:04:23 -0700

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    m_std= int(input('Male students:'))
+    f_std= int(input('Female students:'))
+    total_std= (m_std + f_std)
+    m_perc=(m_std / total_std) *100
+    f_perc=(f_std / total_std) *100
+    no_males_females=(m_std , f_std)
 
+    print("Total number of students:", total_std)
+    print("The number of males and females:", no_males_females)
+    print(f'Percentage of males \t {m_perc:.2f}')
+    print(f'Perecentage of females \t {f_perc:.2f}')
     """
     ########################################
     # Do not delete the return statement
